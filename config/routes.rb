938a6ed4afa93b2ca6d2b8d39a51e2a66cdee39f
Marzena Szopinska <marzena_szopinska@hotmail.com>
2017-03-25 13:12:57 +0000

    added Drag & Drop to sections

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
 
   namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
+
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
